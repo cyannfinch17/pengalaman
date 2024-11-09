66c30aeb2b559f7dc4cb956d5e9b6a6f93df75b3
cyannfinch17 <diaskurniawan395@gmail.com>
2024-11-10 06:56:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Contoh fungsi JavaScript untuk menyapa pengguna
+function greetUser () {
+    alert("Selamat datang di halaman perkenalan diri saya!");
+}
+
+// Memanggil fungsi saat halaman dimuat
+window.onload = greetUser ;
